40908a5ad20822b74a4ca7429e208b47b60e85fb
Andreas Röhrle-Förnzler <andreas.roehrle@web.de>
2026-05-11 17:48:18 +0200

    Add noice plugin

--- init.lua
+++ init.lua
@@ -968,6 +968,7 @@ do
   require 'kickstart.plugins.autopairs'
   -- require 'kickstart.plugins.neo-tree'
   -- require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
+  require 'kickstart.plugins.noice'
 
   -- NOTE: You can add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --
---
+++ lua/kickstart/plugins/command-completion.lua
@@ -0,0 +1,15 @@
+
+vim.pack.add { 'https://github.com/smolck/command-completion.nvim'}
+require('command-completion').setup {
+	border = 'single', -- What kind of border to use, passed through directly to `nvim_open_win()`,
+	-- see `:help nvim_open_win()` for available options (e.g. 'single', 'double', etc.)
+	max_col_num = 1, -- Maximum number of columns to display in the completion window
+	min_col_width = 20, -- Minimum width of completion window columns
+	use_matchfuzzy = false, -- Whether or not to use `matchfuzzy()` (see `:help matchfuzzy()`) 
+	-- to order completion results
+	highlight_selection = true, -- Whether or not to highlight the currently
+	-- selected item, not sure why this is an option tbh
+	highlight_directories = true, -- Whether or not to higlight directories with
+	-- the Directory highlight group (`:help hl-Directory`)
+	tab_completion = true,
+}
