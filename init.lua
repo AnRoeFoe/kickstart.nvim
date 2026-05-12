f40ec1d54116e22891ff17c07091363ed67217b6
Andreas Röhrle-Förnzler <andreas.roehrle@web.de>
2026-05-12 14:37:07 +0200

    Use nvim-tree

--- init.lua
+++ init.lua
@@ -98,6 +98,9 @@ do
   vim.g.mapleader = ' '
   vim.g.maplocalleader = ' '
 
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
+
   -- Set to true if you have a Nerd Font installed and selected in the terminal
   vim.g.have_nerd_font = true
 
@@ -967,9 +970,10 @@ do
   require 'kickstart.plugins.autopairs'
   -- require 'kickstart.plugins.neo-tree'
   -- require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
-  require 'kickstart.plugins.noice'
+  -- require 'kickstart.plugins.noice'
   require 'kickstart.plugins.numbertoggle'
-  require 'kickstart.plugins.neo-tree'
+  -- require 'kickstart.plugins.neo-tree'
+  require 'kickstart.plugins.nvim-tree'
   require 'kickstart.plugins.vim-fugitive'
 
   -- NOTE: You can add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
---
+++ lua/kickstart/plugins/nvim-tree.lua
@@ -0,0 +1,21 @@
+
+vim.pack.add( { 'https://github.com/nvim-tree/nvim-tree.lua' } )
+
+---@type nvim_tree.config
+local config = {
+	sort = {
+		sorter = "case_sensitive",
+	},
+	view = {
+		width = 30,
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
+}
+
+require("nvim-tree").setup(config)
+
